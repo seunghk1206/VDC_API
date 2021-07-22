a5a27da5abb00e3974479ca77f8cb2ce6f6ff506
Seunghyeon Kim <60473607+seunghk1206@users.noreply.github.com>
2021-07-22 20:54:52 +0900

    Create PackageInit.py

---
+++ PackageInit.py
@@ -0,0 +1,8 @@
+import VersatileDeciConverter as VDC
+import VersatileDeciMT64 as VDCM
+
+def VDCInit(Target, nomial):
+    return VDC.versatileDeci(0, Target, nomial)
+
+def VDCMInit(Target, nomial):
+    return VDCM.versatileDeci(0, Target, nomial)
